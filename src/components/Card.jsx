789aa08bca0df13145b04f463ab03dfdb88bb129
Varulli <120419874+Varulli@users.noreply.github.com>
2024-05-20 19:18:03 -0400

    Write function for creating Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,9 @@
-function Card() {
-  return <></>;
+function Card({ image, description, handleClick }) {
+  return (
+    <div className="card" onClick={handleClick}>
+      <img src={image} alt={description + " card"} />
+    </div>
+  );
 }
 
 export default Card;
