278e549f7d864cd9adcdb510c1d66329df327780
Varulli <120419874+Varulli@users.noreply.github.com>
2024-05-19 20:54:00 -0400

    Lay out basic elements of App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,27 @@
 import "../styles/App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="info">
+        <h1>Memory Card</h1>
+        <p>Click consecutive unique cards to increase your score.</p>
+
+        <label htmlFor="number-of-cards">Number of Cards:</label>
+        <input
+          type="number"
+          name="number-of-cards"
+          id="number-of-cards"
+          value={10}
+          min={1}
+          max={100}
+          step={1}
+        />
+      </div>
+
+      <div className="card-grid"></div>
+    </>
+  );
 }
 
 export default App;
