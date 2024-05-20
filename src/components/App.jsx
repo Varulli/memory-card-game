43f2bce99a91af33b00ebce239e332e89a86fe6e
Varulli <120419874+Varulli@users.noreply.github.com>
2024-05-19 21:32:59 -0400

    Write functionality for controlling input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
-// import { useState } from 'react'
+import { useState } from "react";
 import "../styles/App.css";
 
 function App() {
+  const [numCards, setNumCards] = useState(10);
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value);
+    setNumCards(isNaN(value) || value < 1 ? 1 : value > 100 ? 100 : value);
+  };
+
   return (
     <>
       <div className="info">
@@ -13,10 +20,14 @@ function App() {
           type="number"
           name="number-of-cards"
           id="number-of-cards"
-          value={10}
+          value={numCards}
           min={1}
           max={100}
           step={1}
+          onChange={handleInputChange}
+          onKeyDown={(e) =>
+            ["e", "E", "+", "-"].includes(e.key) && e.preventDefault()
+          }
         />
       </div>
 
