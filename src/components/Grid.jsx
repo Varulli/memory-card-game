a3e886c7b93e20b7d660728a6c7e89baf92ccd1c
Varulli <120419874+Varulli@users.noreply.github.com>
2024-05-20 20:09:42 -0400

    Write function to retrieve card data from API

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,4 +1,37 @@
+import { useState, useEffect } from "react";
+
+const cardData = [];
+
+async function getCardData(size) {
+  cardData.length = 0;
+
+  for (let i = 0; i < size; i++) {
+    let id;
+    do {
+      id = Math.floor(Math.random() * 649) + 1;
+    } while (cardData[id]);
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/{id}`);
+    const data = await response.json();
+
+    cardData[id] = {
+      name: data.name,
+      image:
+        data.sprites.versions["generation-v"]["black-white"].animated
+          .front_default,
+      selected: false,
+    };
+  }
+}
+
 function Grid({ size }) {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    getCardData(size);
+  }, [size]);
+
   return <></>;
 }
 
